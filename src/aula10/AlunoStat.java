262d72264697dd1d80eef1afcd5b75503f78fd75
Giovane Barcelos <giovanebarcelos@gmail.com>
2024-03-19 09:53:10 -0300

    feat: cadastro de alunos

---
+++ src/aula10/AlunoStat.java
@@ -0,0 +1,26 @@
+package aula10;
+
+/*
+   Gerenciamento de cadastro de alunos
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AlunoStat {
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        ArrayList<Aluno> alunos =
+                new ArrayList<Aluno>();
+
+        Aluno aluno;
+        do {
+            aluno = new Aluno();
+            System.out.print("Primeiro Nome: ");
+            aluno.nome = ler.next();
+            System.out.print("Idade: ");
+            aluno.idade = ler.nextInt();
+            if (aluno.idade >0){alunos.add(aluno);}
+        } while(aluno.idade > 0);
+    }
+}
